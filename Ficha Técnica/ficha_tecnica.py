1e012d43905523a2c77ff53ef47850eef896dc0c
BrunoTimbo <brunoluis00@gmail.com>
2026-08-05 11:24:42 -0300

    inicio de posicionamento

--- Ficha Técnica/ficha_tecnica.py
+++ Ficha Técnica/ficha_tecnica.py
@@ -58,19 +58,14 @@ def exibir_menu():
 5. Excluir""")
 
         opcao = (input("\nEscolha a opção: "))
-
         if opcao == '1':
-            listar_ingredientes()
-            
+            listar_ingredientes()            
         elif opcao == '2':
             cadastrar_ingrediente()
-
         elif opcao == '3':
-            buscar_ingrediente()            
-
+            buscar_ingrediente()
         elif opcao == '4': 
             atualizar_ingrediente()
-
         elif opcao == '5':
             excluir_ingrediente()
         else:
@@ -79,81 +74,53 @@ def exibir_menu():
 # Inserir dados na tabela ingredientes
 
 def listar_ingredientes():
-
-        cursor.execute('''SELECT COUNT(nome_ingrediente) FROM ingredientes WHERE nome_ingrediente IS'''" NOT NULL;")
-        
+        cursor.execute('''SELECT COUNT(nome_ingrediente) FROM ingredientes WHERE nome_ingrediente IS'''" NOT NULL;")        
         quantidade = cursor.fetchone()[0]
-
         if quantidade > 0:
                 cursor.execute("SELECT nome_ingrediente FROM ingredientes")
-
                 resultados = cursor.fetchall()
-
                 for linha in resultados:
-
                         print(linha[0])
-
         else:
-                print("\nNão há ingredientes cadastrados.")
-
-        
-
+                print("\nNão há ingredientes cadastrados.") 
         while True:
-
                 opcao = input("\nDigite 0 para voltar: ")
-
                 if opcao == '0':
-
                         exibir_menu()
-
                 else:
-
                         print("\nOpção invalida.")
 
 def cadastrar_ingrediente():
-
         while True:
-
                 ingrediente = input("\nDigite o ingrediente a ser cadastrado (0 para voltar): ")
-
                 if ingrediente != '0':
-
                         # Consulta com SELECT EXISTS e parâmetro seguro (?) para evitar SQL Injection
                         cursor.execute('''SELECT EXISTS(SELECT 1 FROM ingredientes WHERE nome_ingrediente = ? LIMIT 1)''', (ingrediente,)
                         )
-
                         # Recupera o resultado da consulta
                         resultado = cursor.fetchone()
-
                         # Se o primeiro valor da tupla for 1, o item existe
                         if resultado[0] == 1:
                                 print(f"O ingrediente '{ingrediente}' já está cadastrado.")
                         else:
                                 cursor.execute('''INSERT INTO ingredientes (nome_ingrediente) VALUES (?)''', (ingrediente,))
-                                
                                 # Confirmar a transação
                                 conexao.commit()
                                 print(f"Ingrediente '{ingrediente}' cadastrado.")
 
                 else:
                        break
-
 def buscar_ingrediente():           
 
         while True:
-
                 ingrediente = input("\nDigite o ingrediente a ser procurado (0 para voltar): ")
-
                 if ingrediente != '0':
-
                         # Consulta com SELECT EXISTS e parâmetro seguro (?) para evitar SQL Injection
                         cursor.execute(
                                '''SELECT EXISTS(SELECT 1 FROM ingredientes WHERE nome_ingrediente = ? LIMIT 1)''', (ingrediente,)
                         )
-
                         # Recupera o resultado da consulta
                         resultado = cursor.fetchone()
-
                         # Se o primeiro valor da tupla for 1, o item existe
                         if resultado[0] == 1:
                                 print(f"O ingrediente '{ingrediente}' está cadastrado.")
@@ -168,50 +135,34 @@ def atualizar_ingrediente():
         while True:
 
                 ingrediente = input("\nDigite o ingrediente a ser atualizado (0 para voltar): ")
-
                 if ingrediente != '0':
-
                         # Consulta com SELECT EXISTS e parâmetro seguro (?) para evitar SQL Injection
                         cursor.execute(
                                 '''SELECT EXISTS(SELECT 1 FROM ingredientes WHERE nome_ingrediente = ? LIMIT 1)''', (ingrediente,)
                         )
-
                         # Recupera o resultado da consulta
                         resultado = cursor.fetchone()
-
                         # Se o primeiro valor da tupla for 1, o item existe
                         if resultado[0] == 1:
-
                                 novo_ingrediente = input("\nDigite o novo nome do ingrediente: ")
-
                                 cursor.execute(
                                        '''UPDATE ingredientes SET nome_ingrediente = ? WHERE nome_ingrediente d= ?''', (novo_ingrediente, ingrediente,)
                                 )
-
                                 conexao.commit()
-
                                 print("\nIngrediente atualizado!")
-
                         else:
                                 print(f"\nO ingrediente '{ingrediente}' não está cadastrado.")
-
                 else:
                         break
 
 def excluir_ingrediente():
-
         while True:
-
                 ingrediente = input("\nDigite o ingrediente a ser deletado (0 para voltar): ")
-
                 if ingrediente != '0':
-
                         cursor.execute(
                                '''SELECT EXISTS(SELECT 1 FROM ingredientes WHERE nome_ingrediente = ? LIMIT 1)''', (ingrediente,)
                         )
-
                         resultado = cursor.fetchone()
-
                         if resultado[0] == 1:
                                 cursor.execute(
                                         '''DELETE FROM ingredientes WHERE nome_ingrediente = ?''', (ingrediente,)
@@ -222,14 +173,18 @@ def excluir_ingrediente():
                                 print(f"O ingrediente '{ingrediente}' não está cadastrado.")
                 else:
                        break
+
 # criação da janela
 janela = tk.Tk()
 janela.title("Ficha Técnica")
 janela.geometry("400x400+100+100")
 janela.resizable(False, False)
 
+# frame
+frame_titulo = tk.Frame(janela)
+
 # label 1
-label_1 = tk.Label(janela, text="Menu")
+label_1 = tk.Label(janela, text="Ficha Técnica")
 label_1.pack()
 
 # tabela
@@ -241,20 +196,20 @@ tabela.heading("ID", text="ID")
 tabela.heading("Ingrediente", text="Ingrediente")
 tabela.column("ID", width=10)
 tabela.column("Ingrediente", width=200)
-
 tabela.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 # exibe dados na tabela
 busca_dados_tabela()
 
-botao_listar = tk.Button(janela, text="Listar", command=listar_ingredientes)
-botao_listar.pack()
 botao_cadastrar = tk.Button(janela, text="Cadastrar", command=cadastrar_ingrediente)
 botao_cadastrar.pack()
+
 botao_buscar = tk.Button(janela, text="Buscar", command=buscar_ingrediente)
 botao_buscar.pack()
+
 botao_atualizar = tk.Button(janela, text="Atualizar", command=atualizar_ingrediente)
 botao_atualizar.pack()
+
 botao_excluir = tk.Button(janela, text="Excluir", command=excluir_ingrediente)
 botao_excluir.pack()
 
